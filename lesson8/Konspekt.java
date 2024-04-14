243f9382d6f15c5a12fad1fb2f4f0cca1df58427
Andrii Bilous <ni0410201900@gmail.com>
2024-04-14 12:00:20 +0300

    дз

---
+++ lesson8/Konspekt.java
@@ -0,0 +1,29 @@
+package lesson8;
+
+public class Konspekt {
+
+/*  Створити клас Конспект. Клас повинен містити такі поля:
+    ПІБ студента;
+    Назва предмету;
+    Кількість сторінок;
+    Рік випуску;
+    Колір обкладинки.
+    Назва закладу, де навчається студент;*/
+
+    private String pIBstudent;{
+        System.out.println("Білий Анатолій Васильович");
+    }
+    private String nameTask;{
+        System.out.println("Висока кухня");
+    }
+    private int kilkStorinoc;
+    private int ageVipusk;
+    private String color;
+    private String nameZaklad;
+
+}
+
+
+
+/* Konspekt (String pIBstudent, String nameTask,int kilkStorinoc, int ageVipusk,
+                     String color, String nameZaklad )*/
