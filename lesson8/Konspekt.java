0a9b5bb6aa1f70565ca432af79b3d8110d3fcca3
Andrii Bilous <ni0410201900@gmail.com>
2024-04-14 12:30:24 +0300

    дз

--- lesson8/Konspekt.java
+++ lesson8/Konspekt.java
@@ -10,20 +10,44 @@ public class Konspekt {
     Колір обкладинки.
     Назва закладу, де навчається студент;*/
 
-    private String pIBstudent;{
+    public String pIBstudent;
+
+    {
         System.out.println("Білий Анатолій Васильович");
     }
-    private String nameTask;{
+
+    public String nameTask;
+
+    {
         System.out.println("Висока кухня");
     }
-    private int kilkStorinoc;
-    private int ageVipusk;
-    private String color;
-    private String nameZaklad;
+
+    public int kilkStorinoc;
+
+    {
+        System.out.println(55);
+    }
+
+    public int ageVipusk;
+
+    {
+        System.out.println(2021 + "age");
+    }
+
+    public String color;
+
+    {
+        System.out.println("Gren");
+    }
+
+    public String nameZaklad;
+
+    {
+        System.out.println("Кухня у шефа Ектора Хіменес Браво");
+    }
 
 }
 
 
 
-/* Konspekt (String pIBstudent, String nameTask,int kilkStorinoc, int ageVipusk,
-                     String color, String nameZaklad )*/
+
