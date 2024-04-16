082a073385ef50ccac533d03aeeb72a82a1224cf
Andrii Bilous <ni0410201900@gmail.com>
2024-04-16 12:46:17 +0300

    дз

---
+++ lesson9/ObgectClacComp.java
@@ -0,0 +1,8 @@
+package lesson9;
+
+public class ObgectClacComp {
+    public static void main(String[] args) {
+        Computer computer = new Computer ("MacOS",27432,16,8);
+        System.out.println(computer);
+    }
+}
